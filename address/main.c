ffa73161b3c71614c7a4d7c220509378ffdeeab4
Yoran Heling <projects@yorhel.nl>
2009-04-09 14:10:42 +0200

    address: Added small framework for accepting connections

--- address/main.c
+++ address/main.c
@@ -3,19 +3,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
+#include <sys/select.h>
 
 #define DEFAULT_UNIX_PATH "/tmp/axum-address"
 #define DEFAULT_ETH_DEV   "eth0"
 #define MAX_CONNECTIONS   10
+#define UNIX_SOCK_BUFFER  2048
 
 #ifndef UNIX_PATH_MAX
 # define UNIX_PATH_MAX 108
 #endif
+#define MAX(x, y) ((x)>(y)?(x):(y))
 
 
 /* node info */
@@ -36,7 +40,13 @@ struct mbn_node_info this_node = {
 /* global variables */
 struct mbn_handler *mbn;
 struct sockaddr_un unix_path;
-int unix_socks[MAX_CONNECTIONS+1];
+int listensock;
+struct s_conn {
+  int sock;
+  int buflen;
+  char state; /* 0=unused, 1=read, 2=write */
+  char *buf[UNIX_SOCK_BUFFER];
+} connections[MAX_CONNECTIONS];
 
 
 void init(int argc, char **argv) {
@@ -47,7 +57,7 @@ void init(int argc, char **argv) {
 
   unix_path.sun_family = AF_UNIX;
   strcpy(unix_path.sun_path, DEFAULT_UNIX_PATH);
-  memset((void *)unix_socks, 0, sizeof(int)*(MAX_CONNECTIONS+1));
+  Memset((void *)connections, 0, sizeof(struct s_conn)*MAX_CONNECTIONS);
   strcpy(ethdev, DEFAULT_ETH_DEV);
 
   /* parse options */
@@ -83,24 +93,72 @@ void init(int argc, char **argv) {
   }
 
   /* initialize UNIX listen socket */
-  if((unix_socks[0] = socket(PF_UNIX, SOCK_STREAM, 0)) < 0) {
+  if((listensock = socket(PF_UNIX, SOCK_STREAM, 0)) < 0) {
     perror("Opening socket");
     mbnFree(mbn);
     exit(1);
   }
-  if(bind(unix_socks[0], (struct sockaddr *)&unix_path, sizeof(struct sockaddr_un)) < 0) {
+  if(bind(listensock, (struct sockaddr *)&unix_path, sizeof(struct sockaddr_un)) < 0) {
     perror("Binding to path");
-    close(unix_socks[0]);
+    close(listensock);
     mbnFree(mbn);
     exit(1);
   }
-  if(listen(unix_socks[0], 5) < 0) {
+  if(listen(listensock, 5) < 0) {
     perror("Listening on socket");
     fprintf(stderr, "Are you sure no other address server is running?\n");
-    close(unix_socks[0]);
+    close(listensock);
     mbnFree(mbn);
     exit(1);
   }
+}
+
+
+int mainloop() {
+  fd_set rd, wr;
+  int i, n, max = 0;
+
+  /* set FDs for select() */
+  FD_ZERO(&rd);
+  FD_ZERO(&wr);
+  FD_SET(listensock, &rd);
+  max = MAX(max, listensock);
+  /*
+  for(i=0;i<MAX_CONNECTIONS;i++)
+    if(connections[i].state > 0) {
+      FD_SET(connections[i].sock, (connections[i].state == 1 ? &rd : &wr));
+      max = MAX(max, connections[i].sock);
+    }
+  */
+
+  /* select() */
+  n = select(max+1, &rd, &wr, NULL, NULL);
+  if(n == 0 || (n < 1 && errno == EINTR))
+    return 0;
+  if(n < 1) {
+    perror("select");
+    return 1;
+  }
+
+  /* accept new connection */
+  if(FD_ISSET(listensock, &rd)) {
+    if((n = accept(listensock, NULL, NULL)) < 0) {
+      perror("Accepting new connection");
+      return 1;
+    }
+    for(i=0;i<MAX_CONNECTIONS;i++)
+      if(connections[i].state == 0)
+        break;
+    if(i == MAX_CONNECTIONS)
+      close(n);
+    else {
+      connections[i].sock = n;
+      connections[i].state = 1;
+      connections[i].buflen = 0;
+    }
+  }
+
+  return 0;
 }
 
 
@@ -108,12 +166,14 @@ int main(int argc, char **argv) {
   int i;
 
   init(argc, argv);
-  sleep(10);
+  while(!mainloop())
+    ;
 
   /* free */
   for(i=0; i<MAX_CONNECTIONS+1; i++)
-    if(unix_socks[i] > 0)
-      close(unix_socks[i]);
+    if(connections[i].state > 0)
+      close(connections[i].sock);
+  close(listensock);
   unlink(unix_path.sun_path);
   mbnFree(mbn);
   return 0;
