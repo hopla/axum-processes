99199b78c19499ab2be1416b5d08958718aefb91
Anton Prins <a.prins@d-r.nl>
2009-07-01 09:45:01 +0200

    sql: Added trigger for change of unique ID
    TODO: update address server to handle the new notify

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -13,15 +13,26 @@ $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER recent_changes_notify AFTER INSERT ON recent_changes FOR EACH STATEMENT EXECUTE PROCEDURE notify_changes();
 
+CREATE OR REPLACE FUNCTION before_addresses_change() RETURNS trigger AS $$
+BEGIN
+  IF TG_OP = 'UPDATE' THEN
+    IF (OLD.id).man <> (NEW.id).man OR (OLD.id).prod <> (NEW.id).prod THEN
+      RAISE EXCEPTION 'Changing (addresses.id).man/.prod is not allowed';
+    END IF;
+    IF (OLD.id).id <> (NEW.id).id AND OLD.firm_major = NEW.firm_major THEN
+      DELETE FROM addresses WHERE (id).man = (NEW.id).man AND (id).prod = (NEW.id).prod AND (id).id = (NEW.id).id AND firm_major = NEW.firm_major;
+      INSERT INTO recent_changes (change, arguments) VALUES ('unique_id_changed', OLD.addr::text);
+    END IF;
+  END IF;
+  RETURN NEW;
+END
+$$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION addresses_changed() RETURNS trigger AS $$
 BEGIN
   IF TG_OP = 'DELETE' THEN
     INSERT INTO recent_changes (change, arguments) VALUES ('address_removed', OLD.addr::text);
   ELSIF TG_OP = 'UPDATE' THEN
-    IF (OLD.id).man <> (NEW.id).man OR (OLD.id).prod <> (NEW.id).prod OR (OLD.id).id <> (NEW.id).id THEN
-      RAISE EXCEPTION 'Changing addresses.id is not allowed';
-    END IF;
     IF OLD.engine_addr <> NEW.engine_addr THEN
       INSERT INTO recent_changes (change, arguments) VALUES ('address_set_engine', NEW.addr::text);
     END IF;
@@ -166,16 +177,17 @@ $$ LANGUAGE plpgsql;
 
 -- T R I G G E R S
 
-CREATE TRIGGER template_change_notify     AFTER DELETE ON templates                       FOR EACH ROW EXECUTE PROCEDURE templates_changed();
-CREATE TRIGGER addresses_change_notify    AFTER DELETE OR UPDATE ON addresses             FOR EACH ROW EXECUTE PROCEDURE addresses_changed();
-CREATE TRIGGER defaults_change_notify     AFTER INSERT OR DELETE OR UPDATE ON defaults    FOR EACH ROW EXECUTE PROCEDURE defaults_changed();
-CREATE TRIGGER node_config_change_notify  AFTER INSERT OR DELETE OR UPDATE ON node_config FOR EACH ROW EXECUTE PROCEDURE node_config_changed();
-CREATE TRIGGER slot_config_notify         AFTER UPDATE ON slot_config                     FOR EACH ROW EXECUTE PROCEDURE slot_config_changed();
-CREATE TRIGGER src_config_notify          AFTER INSERT OR DELETE OR UPDATE ON src_config  FOR EACH ROW EXECUTE PROCEDURE src_config_changed();
-CREATE TRIGGER module_config_notify       AFTER UPDATE ON module_config                   FOR EACH ROW EXECUTE PROCEDURE module_config_changed();
-CREATE TRIGGER buss_config_notify         AFTER UPDATE ON buss_config                     FOR EACH ROW EXECUTE PROCEDURE buss_config_changed();
-CREATE TRIGGER monitor_buss_config_notify AFTER UPDATE ON monitor_buss_config             FOR EACH ROW EXECUTE PROCEDURE monitor_buss_config_changed();
-CREATE TRIGGER extern_src_config_notify   AFTER UPDATE ON extern_src_config               FOR EACH ROW EXECUTE PROCEDURE extern_src_config_changed();
-CREATE TRIGGER talkback_config_notify     AFTER UPDATE ON talkback_config                 FOR EACH ROW EXECUTE PROCEDURE talkback_config_changed();
-CREATE TRIGGER global_config_notify       AFTER UPDATE ON global_config                   FOR EACH ROW EXECUTE PROCEDURE global_config_changed();
+CREATE TRIGGER template_change_notify         AFTER DELETE ON templates                       FOR EACH ROW EXECUTE PROCEDURE templates_changed();
+CREATE TRIGGER before_addresses_change_notify BEFORE UPDATE ON addresses                      FOR EACH ROW EXECUTE PROCEDURE before_addresses_change();
+CREATE TRIGGER addresses_change_notify        AFTER DELETE OR UPDATE ON addresses             FOR EACH ROW EXECUTE PROCEDURE addresses_changed();
+CREATE TRIGGER defaults_change_notify         AFTER INSERT OR DELETE OR UPDATE ON defaults    FOR EACH ROW EXECUTE PROCEDURE defaults_changed();
+CREATE TRIGGER node_config_change_notify      AFTER INSERT OR DELETE OR UPDATE ON node_config FOR EACH ROW EXECUTE PROCEDURE node_config_changed();
+CREATE TRIGGER slot_config_notify             AFTER UPDATE ON slot_config                     FOR EACH ROW EXECUTE PROCEDURE slot_config_changed();
+CREATE TRIGGER src_config_notify              AFTER INSERT OR DELETE OR UPDATE ON src_config  FOR EACH ROW EXECUTE PROCEDURE src_config_changed();
+CREATE TRIGGER module_config_notify           AFTER UPDATE ON module_config                   FOR EACH ROW EXECUTE PROCEDURE module_config_changed();
+CREATE TRIGGER buss_config_notify             AFTER UPDATE ON buss_config                     FOR EACH ROW EXECUTE PROCEDURE buss_config_changed();
+CREATE TRIGGER monitor_buss_config_notify     AFTER UPDATE ON monitor_buss_config             FOR EACH ROW EXECUTE PROCEDURE monitor_buss_config_changed();
+CREATE TRIGGER extern_src_config_notify       AFTER UPDATE ON extern_src_config               FOR EACH ROW EXECUTE PROCEDURE extern_src_config_changed();
+CREATE TRIGGER talkback_config_notify         AFTER UPDATE ON talkback_config                 FOR EACH ROW EXECUTE PROCEDURE talkback_config_changed();
+CREATE TRIGGER global_config_notify           AFTER UPDATE ON global_config                   FOR EACH ROW EXECUTE PROCEDURE global_config_changed();
 CREATE TRIGGER dest_config_notify         AFTER INSERT OR DELETE OR UPDATE ON dest_config FOR EACH ROW EXECUTE PROCEDURE dest_config_changed();
