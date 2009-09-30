a39c566f98891383c84762605c7e5fe554431c64
Anton Prins <a.prins@d-r.nl>
2009-09-30 10:25:07 +0200

    sql: Adjusted src_config_changed() function to update source numbers

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -108,6 +108,23 @@ $$ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION src_config_changed() RETURNS trigger AS $$
 BEGIN
   IF TG_OP = 'DELETE' THEN
+    UPDATE module_config SET source_a = 0 WHERE source_a = OLD.number+288;
+    UPDATE module_config SET source_b = 0 WHERE source_b = OLD.number+288;
+    UPDATE module_config SET source_c = 0 WHERE source_c = OLD.number+288;
+    UPDATE module_config SET source_d = 0 WHERE source_d = OLD.number+288;
+    UPDATE module_config SET insert_source = 0 WHERE insert_source = OLD.number+288;
+    UPDATE dest_config SET source = 0 WHERE source = OLD.number+288;
+    UPDATE dest_config SET mix_minus_source = 0 WHERE mix_minus_source = OLD.number+288;
+    UPDATE extern_src_config SET ext1 = 0 WHERE ext1 = OLD.number+288;
+    UPDATE extern_src_config SET ext2 = 0 WHERE ext2 = OLD.number+288;
+    UPDATE extern_src_config SET ext3 = 0 WHERE ext3 = OLD.number+288;
+    UPDATE extern_src_config SET ext4 = 0 WHERE ext4 = OLD.number+288;
+    UPDATE extern_src_config SET ext5 = 0 WHERE ext5 = OLD.number+288;
+    UPDATE extern_src_config SET ext6 = 0 WHERE ext6 = OLD.number+288;
+    UPDATE extern_src_config SET ext7 = 0 WHERE ext7 = OLD.number+288;
+    UPDATE extern_src_config SET ext8 = 0 WHERE ext8 = OLD.number+288;
+    UPDATE monitor_buss_config SET default_selection = 0 WHERE default_selection = OLD.number+288;
+    UPDATE talkback_config SET source = 0 WHERE source = OLD.number+288;
     INSERT INTO recent_changes (change, arguments) VALUES('src_config_changed', OLD.number::text);
   ELSE
     INSERT INTO recent_changes (change, arguments) VALUES('src_config_changed', NEW.number::text);
